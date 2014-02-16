641ea0740e97a1c82657cc22b39a7757e1446bdf
Nicolas KC <nicolas@kolnescassis.com>
2014-02-16 17:01:04 +0100

    Started on subclass of Person

---
+++ Student.java
@@ -0,0 +1,12 @@
+package oving6;
+
+public class Student extends Person {
+	
+	private String major;
+	private int grade;
+	
+	public Student(String givenName, String surname, Address address) {
+		super(givenName, surname, address);
+	}
+
+}
